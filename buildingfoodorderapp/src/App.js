89bb2b3578fd9d23cb1cca783893319e190a3111
omar saade <omarsaadeh11@gmail.com>
2022-06-21 22:38:48 +0300

    react

--- buildingfoodorderapp/src/App.js
+++ buildingfoodorderapp/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
